91ce8ff0bee0e2e0307577d4a4c64d741bd2a83e
ADISAK PORNCHAROEN <153210762+flukeyl@users.noreply.github.com>
2024-02-20 02:19:05 +0700

    Add files via upload

---
+++ component/img.js
@@ -0,0 +1,19 @@
+function ImgFirst (){
+    const imgFirst = document.getElementById("imgFirst").getAttribute("src");
+    document.getElementById("imgMain").setAttribute("src" , imgFirst)
+}
+
+function ImgSecond (){
+    const imgSecond = document.getElementById("imgSecond").getAttribute("src");
+    document.getElementById("imgMain").setAttribute("src" , imgSecond)
+}
+
+function ImgThird (){
+    const imgThird = document.getElementById("imgThird").getAttribute("src");
+    document.getElementById("imgMain").setAttribute("src" , imgThird)
+}
+
+function ImgFourth (){
+    const imgFourth = document.getElementById("imgFourth").getAttribute("src");
+    document.getElementById("imgMain").setAttribute("src" , imgFourth)
+}
